d17c72cc09f6cee4ad7d862778a2b0fb8770bd15
Cody Wild <codywild@berkeley.edu>
2020-06-02 14:28:29 -0700

    Tests passing
    Former-commit-id: 190b7258988cf9290a0349ef6c2d89026caf2d34

--- drlhp/HumanPreferencesEnvWrapper.py
+++ drlhp/HumanPreferencesEnvWrapper.py
@@ -45,6 +45,7 @@ def _load_or_create_pref_db(prefs_dir, max_prefs, logger):
 
 def run_pref_interface(pref_interface, seg_pipe, pref_pipe, remaining_pairs, kill_processes):
     sys.stdin = os.fdopen(0)
+    print("Running pref interface func")
     pref_interface.run(seg_pipe=seg_pipe,
                        pref_pipe=pref_pipe,
                        remaining_pairs=remaining_pairs,
@@ -65,7 +66,9 @@ def make_reward_predictor(reward_predictor_network, log_dir, obs_shape, logger, 
 def _train_reward_predictor(reward_predictor_network, obs_shape, pref_pipe, reward_training_steps, prefs_dir, max_prefs,
                             ckpt_interval, kill_processes_flag, database_refresh_interval,
                             validation_interval, num_initial_prefs, save_prefs_flag, save_model_flag,
-                            pretrained_reward_predictor_dir, log_dir, log_level):
+                            pretrained_reward_predictor_dir, log_dir, log_level, train_reward, pref_db_size):
+    print("Running reward predictor func")
+
     reward_predictor_logger = logging.getLogger("_train_reward_predictor")
     reward_predictor_logger.setLevel(log_level)
     reward_predictor = make_reward_predictor(reward_predictor_network, log_dir, obs_shape, reward_predictor_logger,
@@ -73,20 +76,27 @@ def _train_reward_predictor(reward_predictor_network, obs_shape, pref_pipe, rewa
 
     pref_buffer = _load_or_create_pref_db(prefs_dir, max_prefs, reward_predictor_logger)
     pref_buffer.start_recv_thread(pref_pipe)
-    pref_db_train, pref_db_val = pref_buffer.get_dbs()
     minimum_prefs_met = False
+
     while True:
+        pref_db_train, pref_db_val = pref_buffer.get_dbs()
+        pref_db_size.value = len(pref_db_train) + len(pref_db_val)
         if save_prefs_flag.value == 1:
             _save_prefs(pref_buffer, log_dir, reward_predictor_logger)
             save_prefs_flag.value = 0
         if kill_processes_flag.value == 1:
             pref_buffer.stop_recv_thread()
             return
+        if not train_reward:
+            # There might be some circumstances where we just want to collect and save preferences (for which we need to create a PrefDB)
+            # but might not want to train a reward model. For those circumstances, we can set train_reward to False
+            continue
+
         if not minimum_prefs_met:
-            pref_db_train, pref_db_val = pref_buffer.get_dbs()
-            if len(pref_db_train) < num_initial_prefs:
-                reward_predictor_logger.info(f"Reward db of length {len(pref_db_train)}, waiting for length {num_initial_prefs} to start training")
-                time.sleep(5)
+            if len(pref_db_train) < num_initial_prefs or len(pref_db_val) < 1:
+                #print(f"Current reward DB sizes: {len(pref_db_train)}, {len(pref_db_val)}")
+                reward_predictor_logger.info(f"Reward dbs of length {len(pref_db_train)}, {len(pref_db_val)}, waiting for length {num_initial_prefs}, 1 to start training")
+                time.sleep(1)
                 continue
             else:
                 minimum_prefs_met = True
@@ -120,6 +130,7 @@ class HumanPreferencesEnvWrapper(Wrapper):
         self.train_reward = train_reward
         self.collect_prefs = collect_prefs
         self.segment_length = segment_length
+        self.segments_collected = 0
         self.preference_interface = preference_interface
         self.reward_predictor_network = reward_predictor_network
         self.pretrained_reward_predictor_dir = pretrained_reward_predictor_dir
@@ -135,11 +146,13 @@ class HumanPreferencesEnvWrapper(Wrapper):
         self.reward_database_refresh_interval = 1
         self.reward_predictor_n_train = 0
         self.reward_predictor_refresh_interval = 20
+        self.using_reward_from_predictor = False
 
         # Create Queues and Values to handle multiprocessing communication
         self.seg_pipe = mp.get_context(self.mp_context).Queue(maxsize=1)
         self.pref_pipe = mp.get_context(self.mp_context).Queue(maxsize=1)
         self.remaining_pairs = mp.get_context(self.mp_context).Value('i', 0)
+        self.pref_db_size = mp.get_context(self.mp_context).Value('i', 0)
         self.kill_pref_interface_flag = mp.get_context(self.mp_context).Value('i', 0)
         self.kill_reward_training_flag = mp.get_context(self.mp_context).Value('i', 0)
         self.save_model_flag = mp.get_context(self.mp_context).Value('i', 0)
@@ -156,12 +169,15 @@ class HumanPreferencesEnvWrapper(Wrapper):
         self.pref_buffer = None
         self.reward_predictor = None
 
+    def reset(self):
         if self.collect_prefs:
             self._start_pref_interface()
-        if self.train_reward:
+        if self.train_reward or self.collect_prefs:
             self._start_reward_predictor_training()
+        return self.env.reset()
 
     def _start_pref_interface(self):
+        print("Should be starting pref interface")
         self.pref_interface_proc = mp.get_context(self.mp_context).Process(target=run_pref_interface, daemon=True,
                                                                            args=(self.preference_interface,
                                                                                  self.seg_pipe, self.pref_pipe,
@@ -170,6 +186,7 @@ class HumanPreferencesEnvWrapper(Wrapper):
         self.pref_interface_proc.start()
 
     def _start_reward_predictor_training(self):
+        print("Should be starting reward predictor function")
         self.reward_training_proc = mp.get_context('fork').Process(target=_train_reward_predictor, daemon=True,
                                                                    args=(self.reward_predictor_network,
                                                                          self.obs_shape,
@@ -186,7 +203,9 @@ class HumanPreferencesEnvWrapper(Wrapper):
                                                                          self.save_model_flag,
                                                                          self.pretrained_reward_predictor_dir,
                                                                          self.log_dir,
-                                                                         self.reward_predictor_log_level))
+                                                                         self.reward_predictor_log_level,
+                                                                         self.train_reward,
+                                                                         self.pref_db_size))
         self.reward_training_proc.start()
 
     def _update_episode_segment(self, obs, reward, done):
@@ -198,8 +217,10 @@ class HumanPreferencesEnvWrapper(Wrapper):
                 self.episode_segment.append(np.copy(obs), 0)
 
         if len(self.episode_segment) == self.segment_length:
+            self.segments_collected += 1
             self.episode_segment.finalise()
             try:
+                #print("Sending segment to pref interface!")
                 self.seg_pipe.put(self.episode_segment, block=False)
             except queue.Full:
                 # If the preference interface has a backlog of segments
@@ -245,6 +266,7 @@ class HumanPreferencesEnvWrapper(Wrapper):
             if should_update_model:
                 self.logger.info("Updating model used for env reward")
             self.load_reward_predictor()
+            self.using_reward_from_predictor = True
         obs, reward, done, info = self.env.step(action)
         if self.collecting_segments:
             self._update_episode_segment(obs, reward, done)
@@ -254,7 +276,8 @@ class HumanPreferencesEnvWrapper(Wrapper):
         else:
             return obs, reward, done, info
 
-    def cleanup_processes(self):
+
+    def _cleanup_processes(self):
         self.logger.debug("Sending kill flags to processes")
         self.kill_reward_training_flag.value = 1
         self.kill_pref_interface_flag.value = 1
@@ -272,3 +295,6 @@ class HumanPreferencesEnvWrapper(Wrapper):
         self.pref_pipe.close()
         self.pref_pipe.join_thread()
 
+    def close(self):
+        self._cleanup_processes()
+        self.env.close()
